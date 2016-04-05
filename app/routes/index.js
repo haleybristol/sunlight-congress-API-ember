86858ef9ca787e536cc8517cc4d42975c06b7acd
Blake Scott and Haley Bristol <Guest@epicodus-71.local>
2016-04-05 14:17:06 -0700

    add ability to display bills awaiting the president's signature on the index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    var url =
+    'http://congress.api.sunlightfoundation.com/bills?history.awaiting_signature=true&apikey=3df4b5a4a1274d68a7e8c68bf0ab0e17';
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+    console.log(responseJSON.results);
+    return responseJSON.results;
+    });
+  },
   actions: {
     zipLookup(params) {
       this.transitionTo('results', params.zip);
